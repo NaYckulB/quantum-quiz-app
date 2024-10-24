35a5f84bdf13a30b68269ad4770a4f4a9446155c
NaYckulB <115214021+NaYckulB@users.noreply.github.com>
2024-10-24 12:05:03 +0300

    Add environment variables and configure next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  };
-  
-  export default nextConfig;
-  
+  env: {
+    API_KEY: process.env.API_KEY,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    SECRET_KEY: process.env.SECRET_KEY,
+  },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.resolve.alias['react-dom$'] = 'react-dom/profiling';
+      config.resolve.alias['scheduler/tracing'] = 'scheduler/tracing-profiling';
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
